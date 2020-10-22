750f8cf61b487c82d8e527156b41931145f06727
Muhammad <mh.saqibpsm@gmail.com>
2020-10-23 00:45:19 +0500

    Morgan logger Template Engine Index Routes Views

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 
@@ -10,6 +12,17 @@ dotenv.config({ path: './config/config.env' })
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
