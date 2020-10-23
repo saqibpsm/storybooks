d51f7d3fd5207da6efc73be716208f287026332d
Muhammad <mh.saqibpsm@gmail.com>
2020-10-23 16:27:43 +0500

    materilize font-awesome static folder login page template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -20,6 +21,10 @@ if (process.env.NODE_ENV === 'development') {
 //handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 // Routes
 app.use('/', require('./routes/index'))
