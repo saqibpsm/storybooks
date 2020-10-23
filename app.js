fccfd704d7c152f7a3dc6ac7ba4ddefbc4eb9ac6
Muhammad <mh.saqibpsm@gmail.com>
2020-10-24 00:12:24 +0500

    Google Login Passport Google Strategy

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+// passport config
+require('./config/passport')(passport)
 
 // Connect MongoDB
 connectDB()
@@ -21,6 +26,17 @@ if (process.env.NODE_ENV === 'development') {
 //handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+// sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')))
