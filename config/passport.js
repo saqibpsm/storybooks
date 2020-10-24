ee22c3ba335080c3517eefec2c2fc525b7239fb9
Muhammad <mh.saqibpsm@gmail.com>
2020-10-24 23:56:55 +0500

    Auth routes profile Data logout Navigation

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const googleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
+const { use } = require('passport')
 const User = require('../models/User')
 
 module.exports = function (passport) {
@@ -9,7 +10,24 @@ module.exports = function (passport) {
         callbackURL: '/auth/google/callback'
     },
         async (accessToken, refreshToken, profile, done) => {
-            console.log(profile)
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value
+            }
+            try {
+                let user = await User.findOne({ googleId: profile.id })
+                if (user) {
+                    done(null, user)
+                } else {
+                    user = await User.create(newUser)
+                    done(null, user)
+                }
+            } catch (err) {
+                console.log(err)
+            }
         }))
 
     passport.serializeUser((user, done) => {
