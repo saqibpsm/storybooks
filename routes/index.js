009b7f9ae40386171a8c338861c7b01a5b65da24
Muhammad <mh.saqibpsm@gmail.com>
2020-10-25 15:14:11 +0500

    from Auth middleware till Dashboard Stories

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middlewares/auth')
+const Story = require('../models/Story')
 
 // @desc    Login/Landing page
 // @route   GET /
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login',
     })
@@ -11,8 +13,19 @@ router.get('/', (req, res) => {
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            displayName: req.user.displayName,
+            stories
+        })
+    } catch (err) {
+        console.log(err)
+        res.render('errors/500')
+    }
+
 })
 
 module.exports = router
